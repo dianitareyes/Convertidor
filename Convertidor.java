8ee99ef86d392302d67ca51a8ce16edd2cc4e0ab
Minorusal <minorusal@hotmail.com>
2024-12-09 19:24:01 -0600

    Se importa la libreria scanner, se hace instancia de la clase scanner y se declara la variable opcion

---
+++ Convertidor.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Convertidor {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+    }
+}            
